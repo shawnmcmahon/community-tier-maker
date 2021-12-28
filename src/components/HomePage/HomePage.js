036a1e2cbe4d59ea5171b4fc87f605e2bf266076
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-27 22:22:04 -0700

    Add styling to row label

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,24 +3,21 @@ import React from 'react'
 const HomePage = () => {
     return (
         <div className="flex flex-col justify-center align-center w-full h-full">
-            <div className="outter-container border-black w-11/12 h-96 bg-black text-white">
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    S
+            <div className="outter-container border-black w-11/12 h-80 bg-black text-white">
+                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                    <div className="row-label bg-red-800 w-20 h-16">S</div>
                 </div>
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    A
+                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                    <div className="row-label bg-orange-500 w-20 h-16">A</div>
                 </div>
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    B
+                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                    <div className="row-label bg-yellow-400 w-20 h-16">B</div>
                 </div>
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    C
+                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                    <div className="row-label bg-yellow-300 w-20 h-16">C</div>
                 </div>
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    D
-                </div>
-                <div className="tier-row text-white bg-gray-900 w-100 h-16">
-                    F
+                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                    <div className="row-label bg-green-600 w-20 h-16">D</div>
                 </div>
             </div>
         </div> 
