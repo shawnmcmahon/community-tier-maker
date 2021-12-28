5d31089d5143bd717b3a72bce4ba7b04ca3a1bde
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-27 22:07:16 -0700

    Add tier rows

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,10 +4,24 @@ const HomePage = () => {
     return (
         <div className="flex flex-col justify-center align-center w-full h-full">
             <div className="outter-container border-black w-11/12 h-96 bg-black text-white">
-                <div className="tier-row text-white bg-gray-900 w-100">
-                    Hello
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    S
                 </div>
-
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    A
+                </div>
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    B
+                </div>
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    C
+                </div>
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    D
+                </div>
+                <div className="tier-row text-white bg-gray-900 w-100 h-16">
+                    F
+                </div>
             </div>
         </div> 
         )
