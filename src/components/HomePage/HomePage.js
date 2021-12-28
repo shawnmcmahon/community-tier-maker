81912c0cc6f4faa4c2607a1292ea62e335293850
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-12-27 22:03:23 -0700

    Install tailwind and begin tier maker

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="flex flex-col justify-center align-center w-full h-full">
+            <div className="outter-container border-black w-11/12 h-96 bg-black text-white">
+                <div className="tier-row text-white bg-gray-900 w-100">
+                    Hello
+                </div>
+
+            </div>
+        </div> 
+        )
+}
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'darkgray': '#333',
+      'black': '#000000',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#evebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    }
+  },
+  plugins: [],
+}
