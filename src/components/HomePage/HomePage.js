3dea68e06823d83bfddb4f4ddc50c2e71a6b4fef
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2022-01-01 12:38:26 -0700

    Add dragandDropcontext and Dropable components

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd';
 
 const HomePage = () => {
 
@@ -18,26 +19,33 @@ const HomePage = () => {
 
     return (
         <div className="flex flex-col justify-center align-center w-full h-full">
-            <div className="outter-container border-black w-11/12 h-80 bg-black text-white">
-                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                    <div className="row-label bg-red-800 w-20 h-16">S</div>
+            <DragDropContext>
+                <Dropable droppableId="tiers"> 
+                    { (provided) => (
+                    
+                    <div className="outter-container border-black w-11/12 h-80 bg-black text-white" {...provided.droppableProps} ref={provided.innerRef}>
+                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                            <div className="row-label bg-red-800 w-20 h-16">S</div>
+                        </div>
+                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16" ondrop={drop} ondragover={allowDrop}>
+                            <div className="row-label bg-orange-500 w-20 h-16">A</div>
+                        </div>
+                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                            <div className="row-label bg-yellow-400 w-20 h-16">B</div>
+                        </div>
+                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                            <div className="row-label bg-yellow-300 w-20 h-16">C</div>
+                        </div>
+                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                            <div className="row-label bg-green-600 w-20 h-16">D</div>
+                        </div>
+                    </div>
+                    )}
+                </Dropable>
+                <div className="item w-20 h-20 bg-black text-white" draggable="true" onDragStart={drag}> 
+                    Item
                 </div>
-                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16" ondrop={drop} ondragover={allowDrop}>
-                    <div className="row-label bg-orange-500 w-20 h-16">A</div>
-                </div>
-                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                    <div className="row-label bg-yellow-400 w-20 h-16">B</div>
-                </div>
-                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                    <div className="row-label bg-yellow-300 w-20 h-16">C</div>
-                </div>
-                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                    <div className="row-label bg-green-600 w-20 h-16">D</div>
-                </div>
-            </div>
-            <div className="item w-20 h-20 bg-black text-white" draggable="true" onDragStart={drag}> 
-                Item
-            </div>
+            </DragDropContext>
         </div> 
         )
 }
