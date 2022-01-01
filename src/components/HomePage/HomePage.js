7f6d86af66d3799287fbb3f075b085f2d4733f43
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2022-01-01 13:22:27 -0700

    Fix issue with Droppable not having a function as the child

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const HomePage = () => {
-    [sodas, setSodas] = useState(["Coke", "Pepsi", "Dr. Pepper", "Fanta Orange", "Fanta Grape", "Rootbeer", "Sprite", "MountainDew"])
+    const [sodas, setSodas] = useState(["Coke", "Pepsi", "Dr. Pepper", "Fanta Orange", "Fanta Grape", "Rootbeer", "Sprite", "MountainDew"])
 
    
     return (
@@ -10,33 +10,43 @@ const HomePage = () => {
             <DragDropContext>
                 <Droppable droppableId="tiers"> 
                     { (provided) => (
-                    <div className="outter-container border-black w-11/12 h-80 bg-black text-white" {...provided.droppableProps} ref={provided.innerRef}>
-                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                            <div className="row-label bg-red-800 w-20 h-16">S</div>
-                        </div>
-                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16" ondrop={drop} ondragover={allowDrop}>
-                            <div className="row-label bg-orange-500 w-20 h-16">A</div>
-                        </div>
-                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                            <div className="row-label bg-yellow-400 w-20 h-16">B</div>
-                        </div>
-                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                            <div className="row-label bg-yellow-300 w-20 h-16">C</div>
-                        </div>
-                        <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
-                            <div className="row-label bg-green-600 w-20 h-16">D</div>
-                        </div>
+                        <div>
+                            <div className="outter-container border-black w-11/12 h-80 bg-black text-white" {...provided.droppableProps} ref={provided.innerRef}>
+                                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                                    <div className="row-label bg-red-800 w-20 h-16">S</div>
+                                </div>
+                                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                                    <div className="row-label bg-orange-500 w-20 h-16">A</div>
+                                </div>
+                                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                                    <div className="row-label bg-yellow-400 w-20 h-16">B</div>
+                                </div>
+                                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                                    <div className="row-label bg-yellow-300 w-20 h-16">C</div>
+                                </div>
+                                <div className="tier-row flex-row text-white bg-gray-900 w-100 h-16">
+                                    <div className="row-label bg-green-600 w-20 h-16">D</div>
+                                </div>
+                            </div>
+                        <ul className="item-list"> 
+                            {sodas.map((currentSoda, index) => {
+                                return (
+                                    <Draggable key={currentSoda} draggableId={currentSoda} index={index}> 
+                                        {(provided) => (
+                                            <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} key={currentSoda} className="item w-20 h-20 bg-black text-white"> 
+                                                {currentSoda}
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                );
+                            })}
+                        </ul>
                     </div>
-                    )}
+                )}
                 </Droppable>
-                <Draggable key={1} draggableId={1} index={index}> 
-                    <div className="item w-20 h-20 bg-black text-white" draggable="true" onDragStart={drag}> 
-                        Item
-                    </div>
-                </Draggable>
             </DragDropContext>
         </div> 
-        )
+    )
 }
 
 export default HomePage;
