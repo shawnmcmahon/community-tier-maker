6bd9e01b9069a217dea72121ff87c24c6e82ad51
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2022-01-01 14:35:51 -0700

    Add onDragEnd prprop to DragDropContext

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,13 +2,9 @@ import React, { useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const HomePage = () => {
-    const [sodas, setSodas] = useState({
-        s: ["Coke", "Pepsi", "Dr. Pepper"],
-        a: ["Fanta Orange", "Fanta Grape"],
-        b: ["Rootbeer", "Sprite", "MountainDew"]
-    })
+    const [sodas, setSodas] = useState(["Coke", "Pepsi", "Dr. Pepper", "Fanta Orange", "Fanta Grape", "Rootbeer", "Sprite", "MountainDew"])
     const [rankedBeverages, setRankedBeverages ] = useState(sodas); 
-
+    
 
     const handleOnDragEnd = (result) => {
         console.log(result)
---
+++ src/components/HomePage/HomePage2.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const HomePage = () => {
+    const [sodas, setSodas] = useState({
+        s: ["Coke", "Pepsi", "Dr. Pepper"],
+        a: ["Fanta Orange", "Fanta Grape"],
+        b: ["Rootbeer", "Sprite", "MountainDew"]
+    })
+    const [rankedBeverages, setRankedBeverages ] = useState(sodas); 
+
+
+    const handleOnDragEnd = (result) => {
+        console.log(result)
+        if (!result) {
+            return
+        }
+        const items = Array.from(rankedBeverages); 
+        const [reorderedItem] = items.splice(result.source.index, 1); 
+        items.splice(result.destination.index, 0, reorderedItem); 
+    
+        setRankedBeverages(items) 
+    }
+
+    
+    return (
+        <DragDropContext 
+            onDragEnd={({destination, source}) => {
+                // // dropped outside the list
+                    if (!result.destination) {
+                        return;
+                    }
+                
+                    setSodas(
+                        reorderQuoteMap({
+                            sodas,
+                            source,
+                            destination,
+                        }),
+                    );
+            }}>
+            {Object.entries(sodas).map(([key, value]) => (
+                <AuthorList
+                internalScroll
+                key={key}
+                listId={key}
+                listType="CARD"
+                quotes={value}
+                />
+            ))}
+        </DragDropContext>
+    )
+}
+
+export default HomePage;
