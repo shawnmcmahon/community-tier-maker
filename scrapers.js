bcee478cf11d9f6a8c0b2ea2e1aac94d99cba355
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2022-01-02 12:38:26 -0700

    Parse out image links from data

--- scrapers.js
+++ scrapers.js
@@ -4,15 +4,15 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
-puppeteer.launch({ headless: true }).then(async browser => {
-  console.log('Running tests..')
-  const page = await browser.newPage()
-  await page.goto('https://bot.sannysoft.com')
-  await page.waitForTimeout(5000)
-  await page.screenshot({ path: 'testresult.png', fullPage: true })
-  await browser.close()
-  console.log(`All done, check the screenshot. ✨`)
-})
+// puppeteer.launch({ headless: true }).then(async browser => {
+//   console.log('Running tests..')
+//   const page = await browser.newPage()
+//   await page.goto('https://bot.sannysoft.com')
+//   await page.waitForTimeout(5000)
+//   await page.screenshot({ path: 'testresult.png', fullPage: true })
+//   await browser.close()
+//   console.log(`All done, check the screenshot. ✨`)
+// })
 
 
 
@@ -31,9 +31,15 @@ async function scrapProduct(url) {
   //   return div.innerHTML
   // })
 
-  const pic = await page.evaluate(el => el.innerHTML, await page.$('#create-image-carousel'));
-  console.log({pic})
-  browser.close();
+  const data = await page.evaluate(el => el.innerHTML, await page.$('#create-image-carousel'));
+  const innerHTMLarr = data.match(/images(.*?).png/g).map(val => {
+    console.log(val)
+  })
+
+  console.log({innerHTMLarr})
+
+  browser.close()
+
 }
 
 scrapProduct('https://tiermaker.com/create/mlb-teams-102331')
